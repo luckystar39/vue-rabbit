000422e096e1076625487572001ec3208113799d
luckystar39 <2276522636@qq.com>
2026-04-19 22:05:41 +0800

    day2-11结束

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
- <el-button type="info">Info</el-button>
+  <!-- 一级路由出口组件 -->
+  <RouterView />
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import './assets/main.css'
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+// 引入初始化样式文件
+import '@/styles/comments.scss'
+
+// 测试接口函数
+import { getCategory } from './apis/testAPI'
+getCategory().then(res => {
+  console.log(res);
+  
+})
 
 const app = createApp(App)
 
